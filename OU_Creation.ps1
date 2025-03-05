0e5e0d7a61882c50bdbc41c8e8b6448261a74aad
ggatewood99 <greg.e.gatewood@outlook.com>
2025-03-05 13:39:44 -0800

    Added Security Group

--- OU_Creation.ps1
+++ OU_Creation.ps1
@@ -3,6 +3,8 @@ Import-Module ActiveDirectory
 $OUName = "London"
 $DomainDN = "DC=Adatum,DC=com"
 $OUPath = "OU=$OUName,$DomainDN"
+$GroupName = "York Users"
+
 
 # Check if the OU already exists
 $existingOU = Get-ADOrganizationalUnit -Filter "DistinguishedName -eq '$OUPath'" -SearchBase $DomainDN
@@ -14,5 +16,17 @@ if ($existingOU) {
     Write-Host "OU created"
 }
 
+
 # List all OUs in the domain
 Get-ADOrganizationalUnit -Filter * -SearchBase $DomainDN | Select-Object Name, DistinguishedName
+
+# Check if the OU already exists
+$existinggroup = Get-Adgroup -Filter "Name -eq '$GroupName'" -SearchBase $OUPath -ErrorAction SilentlyContinue
+
+if ($existinggroup) {
+    Write-Output "group already exists"
+} else {
+    New-ADGroup -Name $GroupName -GroupScope Global -GroupCategory Security -path $OUPath
+    Write-Output "Group created"
+}
+
